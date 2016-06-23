d54ed6a25f511b9711366a0ca4327bebcf8d13c2
Luke Kaalim <lukekaalim@gmail.com>
2016-06-24 00:35:10 +1000

    Added version stamping on builds.

---
+++ questgame/Assets/Scripts/Camera/CameraTracker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Cameras
+{
+	[ExecuteInEditMode]
+	public class CameraTracker : MonoBehaviour
+	{
+		[SerializeField]
+		Transform _target;
+
+		public Transform Target
+		{
+			get
+			{
+				return _target;
+			}
+			set
+			{
+				_target = value;
+			}
+		}
+
+		void Update()
+		{
+			if (_target != null)
+			{
+				transform.LookAt(_target, Vector3.up);
+			}
+		}
+	}
+}
---
+++ questgame/Assets/Scripts/Displays/VersionDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Build;
+using DisplayBinding;
+
+namespace Displays
+{
+	public class VersionDisplay : DisplayBase<VersionInfoContainer>
+	{
+		[SerializeField]
+		Text textToDisplayTo;
+
+		override protected void OnValueChange(VersionInfoContainer oldValue, VersionInfoContainer newValue)
+		{
+			if (newValue != null && newValue.CurrentVersionInfo != null)
+			{
+				textToDisplayTo.text = newValue.CurrentVersionInfo.ToString();
+			}
+		}
+	}
+}
