458768fcc45f3958ccba2d8693b72b999ed03d9f
Luke Kaalim <luke@kaal.im>
2017-05-27 14:42:06 +1000

    Added some list extension helper methods

---
+++ questgame/Assets/Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+	public static class ListExtensions
+	{
+		public static bool WithinBounds<T>(this List<T> list, int index)
+		{
+			return index >= 0 || index <= list.Count - 1;
+		}
+
+		public static bool AtEdgeIndex<T>(this List<T> list, int index)
+		{
+			return index <= 0 || index >= list.Count - 1;
+		}
+	}
+}
