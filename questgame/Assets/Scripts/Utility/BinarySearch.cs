ebfaf9d7062402064f5b99190dc04aad0385f68a
Luke Kaalim <luke@kaal.im>
2017-05-30 00:07:05 +1000

    Implement delegate based binary search algorithm

---
+++ questgame/Assets/Scripts/Utility/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utility
+{
+	public delegate int ComparerDelegate<T>(List<T> list, int index, T target);
+
+	public static class Search
+	{
+		public static int Binary<T>(List<T> list, T target, ComparerDelegate<T> comparer)
+		{
+			int min = 0;
+			int max = list.Count - 1;
+
+
+			int pivot = (min + max) / 2;
+			while (min <= max)
+			{
+				int comparisonResult = comparer(list, pivot, target);
+				if (comparisonResult < 0)
+				{
+					min = pivot + 1;
+					pivot = (min + max) / 2;
+				}
+				else if (comparisonResult > 0)
+				{
+					max = pivot - 1;
+					pivot = (min + max) / 2;
+				}
+				else if (comparisonResult == 0)
+				{
+					return pivot;
+				}
+			}
+			return -1;
+		}
+	}
+}
